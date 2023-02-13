682578124208971c267f4fce0a9592f31b25bd3c
yuchen <yuchenz27@outlook.com>
2023-02-13 09:35:45 +0800

    Initial Commit

---
+++ WitSensorTestProject/WitSensorTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WitSensorTestProjectApp.swift
+//  WitSensorTestProject
+//
+//  Created by Yuchen Zhang on 2023/2/13.
+//
+
+import SwiftUI
+
+@main
+struct WitSensorTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
